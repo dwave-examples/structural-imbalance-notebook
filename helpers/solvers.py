7e9a71f0a381d606a64a4c4423c5665c1ada8f9d
jpasvolsky <jpasvolsky@dwavesys.com>
2018-12-20 09:30:14 -0800

    Update solvers.py in structural imbalance

--- helpers/solvers.py
+++ helpers/solvers.py
@@ -13,30 +13,32 @@
 #    limitations under the License.
 
 import os
+from dwave.cloud import Client
+from dwave.cloud.exceptions import *
 from IPython.display import Markdown, display
 
 def print_markdown(string):
     display(Markdown(string))
 
 def default_solver():
-    my_default_solver = os.getenv('DWAVE_API_SOLVER')
+    with Client.from_config() as client:
+        try:
+            my_default_solver = client.get_solver(qpu=True)
+            ds = "Solver: " + my_default_solver.id
+            print(ds)
+        except SolverNotFoundError:
+            my_default_solver = " "
+            print_markdown("<span style='color:red;font-weight:bold'>No D-Wave solver found.</span>")
+            print("Please check available solvers on the <span style='font-weight:bold'>Leap dashboard</span>.")
     my_default_token  = os.getenv('DWAVE_API_TOKEN')
-    if not my_default_solver or (my_default_solver == "None"):
-       print_markdown("<span style='color:red;font-weight:bold'>No default solver.</span>")
-       ds = False
-    else:
-       ds = "Solver: " + my_default_solver
-       print(ds)
     if not my_default_token or (my_default_token == "None"):
        print_markdown("<span style='color:red;font-weight:bold'>No default API token.</span>")
-       dt = False
+       print("An API token is not set for this environment.")
+       print_markdown("You can find your API token on the <span style='font-weight:bold'>Leap dashboard</span>.")
+       print("Please uncomment the line below and paste in the missing token.")
     else:
        dt = "API Token: " + my_default_token[:10] + "***" + my_default_token[-5:]
        print(dt)
-    if not (dt and ds):
-       print("Minimal default solver configuration (solver name and API token) is not set for this environment.")
-       print("Please uncomment the relevant line(s) below and paste in the missing solver name and/or token.")
-       print_markdown("You can find available solver names and your API token on the <span style='font-weight:bold'>Leap dashboard</span>.")
     return(my_default_solver, my_default_token)
 
 
