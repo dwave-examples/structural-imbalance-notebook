e3e793a8af3c8a4534f23dd30d5ef6589fb7146d
Joel Pasvolsky <jpasvolsky@dwavesys.com>
2022-03-17 14:39:54 -0700

    update helpers.draw (1)

--- helpers/draw.py
+++ helpers/draw.py
@@ -12,32 +12,10 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
-import sys
-
 from collections import defaultdict
 from itertools import product
 
 import networkx as nx
-
-from bokeh.io import show, output_notebook
-from bokeh.models import (
-    Plot, Range1d, MultiLine, Circle, HoverTool, WheelZoomTool, ZoomInTool, ZoomOutTool, PanTool,
-    Row, LabelSet, ColumnDataSource
-)
-from bokeh.models.graphs import EdgesAndLinkedNodes
-try:
-    from bokeh.plotting import from_networkx
-except ImportError:
-    # handle bokeh<=1.4.0 (Python 3.5 and earlier)
-    from bokeh.models.graphs import from_networkx
-
-
-# call output_notebook once on import, so we don't reload bokeh every time.
-me = sys.modules[__name__]
-if not hasattr(me, 'bokeh_loaded'):
-    output_notebook()
-    bokeh_loaded = True
-
 
 def draw(S, position=None, with_labels=False):
     """Plot the given signed social network.
@@ -90,58 +68,22 @@ def draw(S, position=None, with_labels=False):
     # call layout wrapper once with all nodes to store position for calls with partial graph
     position = layout_wrapper(S)
 
-    plot = Plot(plot_width=600, plot_height=400, x_range=Range1d(-1.2, 1.2), y_range=Range1d(-1.2, 1.2))
-    tools = [WheelZoomTool(), ZoomInTool(), ZoomOutTool(), PanTool()]
-    plot.add_tools(*tools)
-    plot.toolbar.active_scroll = tools[0]
+    fig, ax = plt.subplots(figsize=(8, 8))
+    ax.set_ylim([-1.2, 1.2])
+    ax.set_xlim([-1.2, 1.2])
 
-    def get_graph_renderer(S, line_dash):
-        # we need a consistent ordering of the edges
-        edgelist = S.edges()
-        nodelist = S.nodes()
+    # get the colors assigned to each edge based on friendly/hostile
+    edge_color = ['#87DACD' if S[u][v]['sign'] == 1 else '#FC9291' for u, v in edgelist]
 
-        # get the colors assigned to each edge based on friendly/hostile
-        sign_edge_color = ['#87DACD' if S[u][v]['sign'] == 1 else '#FC9291' for u, v in edgelist]
-
-        # get the colors assigned to each node by coloring
-        try:
-            coloring_node_color = ['#4378F8' if nodelist[v]['color'] else '#FFE897' for v in nodelist]
-        except KeyError:
-            coloring_node_color = ['#FFFFFF' for __ in nodelist]
-
-        graph_renderer = from_networkx(S, layout_wrapper)
-
-        circle_size = 10
-        graph_renderer.node_renderer.data_source.add(coloring_node_color, 'color')
-        graph_renderer.node_renderer.glyph = Circle(size=circle_size, fill_color='color')
-
-        edge_size = 2
-        graph_renderer.edge_renderer.data_source.add(sign_edge_color, 'color')
-        try:
-            graph_renderer.edge_renderer.data_source.add([S[u][v]['event_year'] for u, v in edgelist], 'event_year')
-            graph_renderer.edge_renderer.data_source.add(
-                [S[u][v]['event_description'] for u, v in edgelist], 'event_description')
-            plot.add_tools(HoverTool(tooltips=[("Year", "@event_year"), ("Description", "@event_description")],
-                                    line_policy="interp"))
-        except KeyError:
-            pass
-        graph_renderer.edge_renderer.glyph = MultiLine(line_color='color', line_dash=line_dash)
-
-        graph_renderer.inspection_policy = EdgesAndLinkedNodes()
-
-        return graph_renderer
-
+    # get the colors assigned to each node by coloring
     try:
-        S_dash = S.edge_subgraph(((u, v) for u, v in edgelist if S[u][v]['frustrated']))
-        S_solid = S.edge_subgraph(((u, v) for u, v in edgelist if not S[u][v]['frustrated']))
-        plot.renderers.append(get_graph_renderer(S_dash, 'dashed'))
-        plot.renderers.append(get_graph_renderer(S_solid, 'solid'))
+        node_color = ['#4378F8' if nodelist[v]['color'] else '#FFE897' for v in nodelist]
+        edge_style = ['dashed' if S[u][v]['frustrated'] else 'solid' for u, v in edgelist]
     except KeyError:
-        plot.renderers.append(get_graph_renderer(S, 'solid'))
+        node_color = ['#FFFFFF' for __ in nodelist]
+        edge_style = "solid"
 
-
-
-    plot.background_fill_color = "#202239"
+    circle_size = 50
 
     positions = layout_wrapper(S)
     if with_labels:
@@ -160,6 +102,10 @@ def draw(S, position=None, with_labels=False):
                         x_offset=-5, y_offset=10, text_color="#F5F7FB", text_font_size='12pt')
         plot.add_layout(labels)
 
-    show(Row(plot))
+    nx.draw_networkx_edges(S, pos=position, edgelist=edgelist, edge_color=edge_color, style=edge_style)
+    nodes = nx.draw_networkx_nodes(S, pos=position, node_color=node_color, node_size=circle_size)
+    nx.draw_networkx_labels(S, pos=position)
+
+    plt.show()
 
     return positions
